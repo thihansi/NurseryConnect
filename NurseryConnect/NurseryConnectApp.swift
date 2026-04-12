f346b535b4ed120216b6597b3ac14710cf40b833
thihansi <thihansig@gmail.com>
2026-04-12 16:20:02 +0530

    Initial Commit

---
+++ NurseryConnect/NurseryConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  NurseryConnectApp.swift
+//  NurseryConnect
+//
+//  Created by Thihansi Gunawardena on 2026-04-12.
+//
+
+import SwiftUI
+
+@main
+struct NurseryConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
